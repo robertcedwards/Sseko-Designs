4acf1769aac68b8e53535c3bdee091ad54804d3b
Robert Edwards <robertcedwards@coactlab.com>
2011-08-30 17:13:10 -0700

    first commit

---
+++ _/js/functions.js
@@ -0,0 +1,23 @@
+// remap jQuery to $
+(function($){})(window.jQuery);
+
+
+/* trigger when page is ready */
+$(document).ready(function (){
+
+    $.backstretch("wp-content/themes/Sseko/images/bg.jpg");
+
+});
+
+
+/* optional triggers
+
+$(window).load(function() {
+	
+});
+
+$(window).resize(function() {
+	
+});
+
+*/
